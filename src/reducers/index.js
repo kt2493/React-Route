5979e4efe44f74a02cbc7199cef3482f343e8947
Krithika <p.krithika93@gmail.com>
2017-12-03 22:55:56 +0530

    route added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-
+import PostsReducer from './postsReducer';
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  posts: PostsReducer  
 });
 
 export default rootReducer;
